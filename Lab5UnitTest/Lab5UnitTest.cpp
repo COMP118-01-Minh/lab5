fc97eb16645048db70bae6536309f880cd811dcc
Minh Nguyen Truong Quang <minhntq18@saigontech.edu.vn>
2024-05-27 10:02:38 +0300

    Unit Test commit

---
+++ Lab5UnitTest/Lab5UnitTest.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5/Matrix.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab5UnitTest
+{
+	TEST_CLASS(Lab5UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethodSumOfRows)
+		{
+			// Arrange
+			const int ROW = 5;
+
+			// Act
+			// Assert
+		}
+	};
+}
